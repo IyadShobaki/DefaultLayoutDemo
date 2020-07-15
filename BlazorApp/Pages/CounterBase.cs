04be41948beee11653d514d97332f60931f04c3f
IyadShobaki <35826354+IyadShobaki@users.noreply.github.com>
2020-07-14 20:59:06 -0400

    Initial commit

---
+++ BlazorApp/Pages/CounterBase.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Pages
+{
+    public class CounterBase : ComponentBase
+    {
+        
+        protected int currentCount = 0;
+
+        [Inject]
+        public ILogger<CounterBase> Log { get; set; }
+
+
+        protected void IncrementCount()
+        {
+            currentCount+=2;
+            Log.LogInformation("The new value is {CounterValue}", currentCount);
+        }
+    }
+}
---
+++ BlazorApp/Pages/FetchData.cs
@@ -0,0 +1,22 @@
+﻿using BlazorApp.Data;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Pages
+{
+    public partial class FetchData
+    {
+        private WeatherForecast[] forecasts;
+
+        [Inject]
+        public WeatherForecastService ForecastService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+        }
+    }
+}
